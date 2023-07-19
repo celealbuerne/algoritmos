b5e3e7d437ebd68ee34510acca2be29dc0911e4b
celealbuerne <131714943+celealbuerne@users.noreply.github.com>
2023-07-19 15:13:11 -0300

    Update TP-Ayed.py

--- TP-Ayed.py
+++ TP-Ayed.py
@@ -2,19 +2,21 @@ contador=0
 per=0
 ind=0
 com=0
+#ListaUser = ["admin","locala","localb","cliente"]*4
+#ListaContra = ["123","a","b","33"] "estas 2 pueden ser 1 array bidimensianal. Los otros 2 supongo que es uni"
 
 def principal():
 	maxim=3
 	global contador
-	c=str("12345")
-	u=str("admin@shopping.com")
+	c=str("admin")
+	u=str("admin")
 	while contador < 3:
 		import getpass
 		user=input("Ingrese su usuario: ")
 		password = getpass.getpass("Ingrese su contraseña: ")  
 		if password==c and user == u:
-			print("Bienvenido")
-			inicio()
+			contador = 5
+			menuop1()
 		else: 
 			print("Datos incorrectos")
 			contador=contador+1
@@ -22,10 +24,6 @@ def principal():
 			print("Le quedan ", maxim, "intentos. Si supera los 3 intentos se cerrara el programa")
 		if contador==3:
 			print("Ha superado el límite de intentos. Se cierra el programa.")
-
-def inicio():
-	menuprincipal()
-	menuop1()
 
 def construccion():
 	print("En construcción...")
@@ -40,42 +38,45 @@ def menuprincipal():
 	print("Elija un número de acuerdo a la sección a la que quiera ingresar.")
 
 def menuop1():
-	global contador
+	global opcion1
+	print("Bienvenido!")
+	menuprincipal()
 	opcion1= str(input("Su opción es: "))
 	while opcion1<"0" or opcion1>"5":
 		print("Ingreso de número incorrecto. Vuelva a intentarlo.")
-		menuprincipal()
 		opcion1= str(input("Su opción es: "))
-	if opcion1=="1":
-		gestionlocal()
-	elif opcion1=="4":
-		gestionnovedades()
-	elif opcion1=="0":
-		contador=5
-	else:
-		construccion()
-		volvermenu()
-		
+	while opcion1 != "0":
+		if opcion1=="1":
+			locales()
+		elif opcion1=="4":
+			novedades()
+		else:
+			construccion()
+		opcion1= str(input("Su opción es: "))
+		while opcion1<"0" or opcion1>"5":
+			print("Ingreso de número incorrecto. Vuelva a intentarlo.")
+			opcion1= str(input("Su opción es: "))		
 
-def GestionlocalNovedades():
+def GestionNovedades():
 	print("[1] Crear Novedades")
 	print("[2] Modificar Novedad")
 	print("[3] Eliminar Novedad")
 	print("[4] Ver Reporte de Novedades")
 	print("[5] Volver") 
 
-def gestionnovedades():
-	GestionlocalNovedades()
+def novedades():
+	GestionNovedades()
 	opcionalt=str(input("Ingrese el número de la sección a la que quiera ingresar:"))
 	while opcionalt<"1" or opcionalt>"5":
 		print("Opción invalida. Vuelva a intentarlo.")
-		GestionlocalNovedades()
 		opcionalt=str(input("Ingrese el número de la sección a la que quiera ingresar: "))
-	if opcionalt=="5":
-		inicio()
-	else:
+	while opcionalt != "5":
 		construccion()
-		volvermenu()	
+		opcionalt=str(input("Ingrese el número de la sección a la que quiera ingresar:"))
+		while opcionalt<"1" or opcionalt>"5":
+			print("Opción invalida. Vuelva a intentarlo.")
+			opcionalt=str(input("Ingrese el número de la sección a la que quiera ingresar: "))
+	menuprincipal()
 
 def Gestionlocalopciones():
 	print("[1] Crear Local")
@@ -83,26 +84,23 @@ def Gestionlocalopciones():
 	print("[3] Eliminar Local")
 	print("[4] Volver") 
 
-def gestionlocal():
+def locales():
 	Gestionlocalopciones()
-	opcion2=str(input("Ingrese el número de la sección a la que quiera ingresar: "))
-	while opcion2<"1" or opcion2>"4":
-		print("Opción inválida. Vuelva a intentarlo")
+	opcion2=input("Ingrese el número de la seccion a la que quiere entrar:")
+	while opcion1<"0" or opcion1>"4":
+		print("Ingreso de número incorrecto. Vuelva a intentarlo.")
+		opcion2=input("Ingrese el número de la seccion a la que quiere entrar:")
+	while opcion2 !="4":
+		if opcion2=="1":
+			crearlocal()
+		else:
+			construccion()
 		Gestionlocalopciones()
-		opcion2=str(input("Ingrese un número de acuerdo a la opción que quiera seleccionar."))
-	if opcion2=="1":
-		crearlocal()
-	elif opcion2=="4":
-		inicio()
-	else:
-		construccion()
-		volvermenu()
-		
-
-def opcioncrear():
-	print("Quiere crear un local?")
-	print("[1] Sí")
-	print("[2] No")
+		opcion2=input("Ingrese el número de la seccion a la que quiere entrar:")
+		while opcion1<"0" or opcion1>"4":
+			print("Ingreso de número incorrecto. Vuelva a intentarlo.")
+			opcion2=input("Ingrese el número de la seccion a la que quiere entrar:")
+	menuprincipal()
 
 def rubros():
 	print("Indique a que tipo de rubro pertenece el local.")
@@ -111,40 +109,28 @@ def rubros():
 	print("[c] Comida")
 
 def crearlocal():
-	opcioncrear()
-	opcion3=str(input("Elija una opción:"))
-	while opcion3!="1" and opcion3!="2":
-		print("Opción inválida. Vuelva a intentarlo.")
-		opcioncrear()
-		opcion3=str(input("Elija una opción:"))
-	while opcion3=="1":
-		localA()
-		opcioncrear()
-		opcion3=str(input("Elija una opción:"))
-		while opcion3!="1" and opcion3!="2":
-			print("Opción invalida. Vuelva a intentarlo.")
-			opcioncrear()
-			opcion3=str(input("Elija una opción:"))
-	if opcion3=="2":
-		localB()
-		volvermenu()
+	print("IMPORTANTE: Si pone un * en el nombre del local, terminara la carga de locales.")
+	localA()
+	localB()
 	
 def localA():
 	global com, per, ind
 	nombrelocal=input("Ingrese el Nombre del local: ")
-	ubilocal=input("Ingrese la Ubicación de local: ")
-	rubros()
-	opcion4= str(input("Elija el tipo de rubro e ingrese la letra del rubro a la que pertenece el local: "))
-	while opcion4<"a" or opcion4>"c":
-		print("Datos incorrectos. Vuelva a intentarlo.")
+	while nombrelocal!="*":
+		ubilocal=input("Ingrese la Ubicación de local: ")
 		rubros()
 		opcion4= str(input("Elija el tipo de rubro e ingrese la letra del rubro a la que pertenece el local: "))
-	if opcion4=="c":
-		com=com+1
-	elif opcion4=="b":
-		per=per+1
-	else:
-		ind=ind+1
+		while opcion4<"a" or opcion4>"c":
+			print("Datos incorrectos. Vuelva a intentarlo.")
+			rubros()
+			opcion4= str(input("Elija el tipo de rubro e ingrese la letra del rubro a la que pertenece el local: "))
+		if opcion4=="c":
+			com=com+1
+		elif opcion4=="b":
+			per=per+1
+		else:
+			ind=ind+1
+		nombrelocal=input("Ingrese el Nombre del local: ")
 
 def localB():
 	global com, per, ind
@@ -159,16 +145,13 @@ def localB():
 		if (com>ind):
 			print("El rubro que menos locales tiene es el de Indumentaria con ", ind, "locales.")
 		else:
-			print("El rubro que menos locales tiene es el de Comida con ", com, "localesss.")
-	else: 
+			print("El rubro que menos locales tiene es el de Comida con ", com, "locales.")
+	else:
 		print("El rubro que más locales tiene es el de Indumentaria con ",ind,"locales.")
 		if (per>com):
-			print("El rubro que menos locales tiene es el de Comida con ", com, "localesss.")
+			print("El rubro que menos locales tiene es el de Comida con ", com, "locales.")
 		else:
 			print("El rubro que menos locales tiene es el de Perfumería con ", per, "locales.")
 
-def volvermenu():
-	print("Va a volver al menú principal")
-	inicio()
-	
+
 principal()
